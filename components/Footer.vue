b7f6d58bc660dbca241f348d7b23f84a8cd6a0ab
Mathilde Arconte <signup@marconte.fr>
2019-07-09 15:43:42 +0900

    debug: jquery

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,19 +2,7 @@
   <div>
     <nav class="navbar navbar-expand-lg">
       <div class="container">
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div>
           <ul class="navbar-nav mr-auto">
             <li class="nav-item copyright">©Aircall</li>
             <li class="nav-item">
